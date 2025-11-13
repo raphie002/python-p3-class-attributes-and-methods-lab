b2769cb43d4233c90e009826bcec20a3772def09
Raphael Njoroge <raphael.njoroge@student.moringaschool.com>
2025-11-13 21:49:23 +0300

    First commit

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,21 @@
 class Song:
-    pass
+    count = 0
+    genre_count = {}
+    artist_count = {}
+    genres = set()
+    artists = set()
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.count += 1
+
+        Song.genres.add(genre)
+        
+        Song.artists.add(artist)
+
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
